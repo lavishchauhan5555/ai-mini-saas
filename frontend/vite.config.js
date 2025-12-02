2cdd56f4d365db895736e52505d34cc8841a01ff
lavishchauhan5555 <lavishchauhan524@gmail.com>
2025-12-02 20:15:14 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import path from "path";
+  
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
-})
+export default ({ mode }) => {
+  // Load env from project root
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react(),  tailwindcss(),],
+    define: {
+      "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
+      "import.meta.env.VITE_SUPABASE_ANON": JSON.stringify(env.VITE_SUPABASE_ANON),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+  });
+};
